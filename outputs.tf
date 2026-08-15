798c3e635d7567d2a432e6b986f902e044744572
Daniel Allen <dan@aeternagroup.net>
2026-08-15 02:17:30 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "ssoadmin_customer_managed_policy_attachments_id" {
 }
 output "ssoadmin_customer_managed_policy_attachments_customer_managed_policy_reference" {
   description = "Map of customer_managed_policy_reference values across all ssoadmin_customer_managed_policy_attachments, keyed the same as var.ssoadmin_customer_managed_policy_attachments"
-  value       = { for k, v in aws_ssoadmin_customer_managed_policy_attachment.ssoadmin_customer_managed_policy_attachments : k => v.customer_managed_policy_reference if v.customer_managed_policy_reference != null && length(v.customer_managed_policy_reference) > 0 }
+  value       = { for k, v in aws_ssoadmin_customer_managed_policy_attachment.ssoadmin_customer_managed_policy_attachments : k => one(v.customer_managed_policy_reference) if v.customer_managed_policy_reference != null && length(v.customer_managed_policy_reference) > 0 }
 }
 output "ssoadmin_customer_managed_policy_attachments_instance_arn" {
   description = "Map of instance_arn values across all ssoadmin_customer_managed_policy_attachments, keyed the same as var.ssoadmin_customer_managed_policy_attachments"
